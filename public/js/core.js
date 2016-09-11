2f68ba5f589aa857f48a0d362550fca68fd62a47
PatrickWeaver <pjpweaver@gmail.com>
2016-09-11 18:32:44 -0400

    keywords on scripts working

--- public/js/core.js
+++ public/js/core.js
@@ -3,8 +3,8 @@ var count  = 0;
 var script_count = 0;
 var special = 1
 var doneChatting = false;
-var keywords;
-var switch_to;
+var keyword_set = {};
+var persist_keywords = false;
 
 var current_script;
 var script_length;
@@ -52,36 +52,52 @@ app.controller('mainController', function($scope, $http, $timeout, $location, $a
 	getBotMessage = function(userMessage) {
 		console.log("## getBotMessage");
 		// Runs when a user inputs a message
+		keyword_found = false;
 
-		/*
-		for (k in keywords) {
-			if (userMessage.includes(keywords[k])){
-				console.log("@ keyword found");
-				apiCall("GET", "scripts", "_id", switch_to);
-				break
-			} else {
-				console.log("@ no keyword")
+		for (from_chat_script in keyword_set)	{
+			for (i in keyword_set[from_chat_script]) {
+				for (to_chat in keyword_set[from_chat_script][i]) {
+					for(keyword in keyword_set[from_chat_script][i][to_chat]){
+						if (to_chat != current_script._id){
+							if (userMessage.toUpperCase().includes(keyword_set[from_chat_script][i][to_chat][keyword].toUpperCase())){
+								console.log("@ keyword found");
+								keyword_found = true;
+								if (!persist_keywords) {
+									key_to_delete = String(current_script._id)
+									console.log("** delete keywords from: " + key_to_delete);
+									delete keyword_set[key_to_delete];
+								}
+								apiCall("GET", "scripts", "_id", to_chat);
+								break
+							} else {
+								console.log("@ no keyword")
+							}
+						}
+					}
+				}
 			}
 		}
-		*/
 
-		if (script_length > script_count) {
-			console.log(" || On " + script_count + " of " + script_length + " in script.");
-			sendToBot();
-		} else {
-			if (special != 2) {
-				// Go to pre-goodbye or goodbye script
-				console.log(" || On " + script_count + " of " + script_length + " in script. I am out of things to say.");
-				script_count = 0;
-				special = 2; // Set special to 2 to start Goodbye script
-				// Need to prevent goodbye loop, or store the special in a variable?
-				apiCall("GET", "scripts", "special", special)
+		if (!keyword_found){
+
+			if (script_length > script_count) {
+				console.log(" || On " + script_count + " of " + script_length + " in script.");
+				sendToBot();
 			} else {
-				doneChatting = true;
-				who = "user";
-				console.log("else else " + script_count + " of " + script_length + " Special: " + special)
+				if (special != 2) {
+					// Go to pre-goodbye or goodbye script
+					console.log(" || On " + script_count + " of " + script_length + " in script. I am out of things to say.");
+					script_count = 0;
+					special = 2; // Set special to 2 to start Goodbye script
+					// Need to prevent goodbye loop, or store the special in a variable?
+					apiCall("GET", "scripts", "special", special)
+				} else {
+					doneChatting = true;
+					who = "user";
+					console.log("else else " + script_count + " of " + script_length + " Special: " + special)
+				}
+				
 			}
-			
 		}
 
 	}
@@ -98,7 +114,6 @@ app.controller('mainController', function($scope, $http, $timeout, $location, $a
 			bot_message = current_script.chats[script_count];
 		}
 		$timeout(function() {
-			console.log("sendToBot() TIMEOUT")
 			send(who, bot_message)
 			script_count += 1;
 		}, wait);
@@ -108,6 +123,10 @@ app.controller('mainController', function($scope, $http, $timeout, $location, $a
 
 	apiCall = function(reqType, model, key, value) {
 		console.log("## apiCall()");
+
+		persist_keywords = false;
+		script_count = 0;
+
 		url = "/api/" + model + "?" + key + "=" + value;
 		console.log("Called: " + url);
 
@@ -123,6 +142,21 @@ app.controller('mainController', function($scope, $http, $timeout, $location, $a
 				randomScript = Math.floor((Math.random() * numberOf)); 
 				current_script = api_response[randomScript];
 				script_length = current_script.chats.length;
+				key = current_script._id;
+				keyword_set[key] = current_script.keywords;
+
+				if (current_script.special === 1) {
+					persist_keywords = true;
+				}
+				
+				for (from_chat_script in keyword_set)	{
+					for (i in keyword_set[from_chat_script]) {
+						for (to_chat in keyword_set[from_chat_script][i]) {
+							console.log("? Keywords from Chat [" + from_chat_script + "] (i)" + i + ":" + '\n' + "To Chat [" + to_chat + "]: " + keyword_set[from_chat_script][i][to_chat]);
+						}
+					}
+				}
+				console.log("-> Chats Loaded: " + current_script.chats)
 				sendToBot();
 
 			} else {
